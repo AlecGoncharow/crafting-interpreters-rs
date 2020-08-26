0d7dd933012b8c5fd4829fc34f06a74f3aae9c00
Alec Goncharow <Alec.Goncharow@gmail.com>
2020-08-25 22:28:24 -0400

    add more resolver stuff

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,111 +1,126 @@
 use crate::ast::{BinaryExpr, Expr, LogicalExpr, Statement, StatementBlock, UnaryExpr};
 use crate::environment::Environment;
-use crate::interpreter::{Callable, Executable, Interpretable, Interpreter, Value};
+use crate::interpreter::Interpreter;
 use crate::token::Token;
-use crate::token::TokenKind;
 use crate::token::TokenLiteral;
 use std::collections::HashMap;
 
+pub enum ResolveError {
+    ScopeError(Token, String),
+}
+
 pub trait Resolvable {
-    fn resolve(&self, resolver: &mut Resolver);
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError>;
 }
 
 impl Resolvable for Expr {
-    fn resolve(&self, resolver: &mut Resolver) {
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
         match self {
             Expr::Assign(token, expr) => {
                 unimplemented!();
             }
-            Expr::Binary(inner) => inner.resolve(resolver),
-            Expr::Unary(inner) => inner.resolve(resolver),
+            Expr::Binary(inner) => inner.resolve(resolver)?,
+            Expr::Unary(inner) => inner.resolve(resolver)?,
 
-            Expr::Call(callee, paren, arguments) => {
-                unimplemented!();
+            Expr::Call(callee, _paren, arguments) => {
+                callee.resolve(resolver)?;
+
+                for expr in arguments {
+                    expr.resolve(resolver)?;
+                }
             }
 
-            Expr::Logical(inner) => inner.resolve(resolver),
+            Expr::Logical(inner) => inner.resolve(resolver)?,
 
-            Expr::Grouping(expression) => expression.resolve(resolver),
-            Expr::Literal(literal) => unimplemented!(),
+            Expr::Grouping(expression) => expression.resolve(resolver)?,
+            Expr::Literal(_literal) => (),
             Expr::Variable(token) => {
+                if !resolver.scopes.is_empty()
+                    && *resolver
+                        .scopes
+                        .first()
+                        .unwrap()
+                        .get(&token.lexeme)
+                        .unwrap_or(&true)
+                        == false
+                {
+                    return Err(ResolveError::ScopeError(
+                        token.clone(),
+                        "Cannot read local variable in its own initalizer".into(),
+                    ));
+                }
                 unimplemented!();
+                // requires a reference to the interpreter, need to think about this part more
             }
         }
+        Ok(())
     }
 }
 
 impl Resolvable for BinaryExpr {
-    fn resolve(&self, resolver: &mut Resolver) {
-        unimplemented!();
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
+        self.left.resolve(resolver)?;
+        self.right.resolve(resolver)
     }
 }
 
 impl Resolvable for UnaryExpr {
-    fn resolve(&self, resolver: &mut Resolver) {
-        unimplemented!();
-        match self.operator.kind {
-            TokenKind::MINUS => unimplemented!(),
-            TokenKind::BANG => unimplemented!(),
-            _ => unreachable!(),
-        }
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
+        self.expr.resolve(resolver)
     }
 }
 
 impl Resolvable for LogicalExpr {
-    fn resolve(&self, resolver: &mut Resolver) {
-        match self.operator.kind {
-            TokenKind::OR => {
-                unimplemented!();
-            }
-
-            TokenKind::AND => {
-                unimplemented!();
-            }
-
-            _ => unreachable!(),
-        }
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
+        self.left.resolve(resolver)?;
+        self.right.resolve(resolver)
     }
 }
 
 impl Resolvable for Statement {
-    fn resolve(&self, resolver: &mut Resolver) {
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
         match self {
-            Statement::Expr(expr) | Statement::ForIncr(expr) => expr.resolve(resolver),
+            Statement::Expr(expr) | Statement::ForIncr(expr) => expr.resolve(resolver)?,
             Statement::If(cond, then_branch, else_branch) => {
-                unimplemented!();
+                cond.resolve(resolver)?;
+                then_branch.resolve(resolver)?;
+                else_branch.resolve(resolver)?;
             }
             Statement::Function(name, args, body, _env) => {
                 unimplemented!();
             }
             Statement::Print(expr) => {
-                unimplemented!();
+                expr.resolve(resolver)?;
             }
             Statement::Var(token, expr) => {
                 resolver.declare(token);
                 if expr != &Expr::Literal(TokenLiteral::Uninit) {
-                    expr.resolve(resolver);
+                    expr.resolve(resolver)?;
                 }
                 resolver.define(token);
             }
             Statement::While(expr, stmt) => loop {
-                unimplemented!();
+                expr.resolve(resolver)?;
+                stmt.resolve(resolver)?;
             },
-            Statement::Block(block) => block.resolve(resolver),
+            Statement::Block(block) => block.resolve(resolver)?,
             Statement::Return(_keyword, value) => {
-                unimplemented!();
+                value.resolve(resolver)?;
             }
         }
+        Ok(())
     }
 }
 
 impl Resolvable for StatementBlock {
-    fn resolve(&self, resolver: &mut Resolver) {
+    fn resolve(&self, resolver: &mut Resolver) -> Result<(), ResolveError> {
         resolver.begin_scope();
         // make inner env our new env
         for stmt in &self.statements {
-            stmt.resolve(resolver);
+            stmt.resolve(resolver)?;
         }
         resolver.end_scope();
+        Ok(())
     }
 }
 
@@ -114,7 +129,9 @@ pub struct Resolver {
 }
 
 impl Resolver {
-    fn resolve(&mut self, interpreter: &mut Interpreter) {}
+    fn resolve(&mut self, interpreter: &mut Interpreter) -> Result<(), ResolveError> {
+        Ok(())
+    }
 
     fn begin_scope(&mut self) {
         self.scopes.push(HashMap::new());
